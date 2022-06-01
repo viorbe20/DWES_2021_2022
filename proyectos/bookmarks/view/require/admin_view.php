7cab6573150d5330623362f75966609f9b718c41
Virginia-Ordoño-Bernier <viorbe20@gmail.com>
2022-06-01 10:48:53 +0200

    feat: bookmark tabla usuarios bloqueados. radiobutton sin terminar

--- proyectos/bookmarks/view/require/admin_view.php
+++ proyectos/bookmarks/view/require/admin_view.php
@@ -7,8 +7,7 @@ if ($_SESSION['user']['profile'] == "admin") {
 
 ?>
     <div id="table_container">
-
-
+        
         <h2>Usuarios bloqueados</h2>
 
         <table>
@@ -19,19 +18,24 @@ if ($_SESSION['user']['profile'] == "admin") {
                 <th>Email</th>
                 <th>Desbloquear</th>
             </tr>
-            
-            
+
+
             <?php
             foreach ($data[0] as $key => $value) {
                 echo '<tr>';
-                echo '<td>'. $value["nombre"] .'</td>';
-                echo '<td>'. $value["user"] .'</td>';
-                echo '<td>'. $value["psw"] .'</td>';
-                echo '<td>'. $value["email"] .'</td>';
+                echo '<td>' . $value["nombre"] . '</td>';
+                echo '<td>' . $value["user"] . '</td>';
+                echo '<td>' . $value["psw"] . '</td>';
+                echo '<td>' . $value["email"] . '</td>';
+                echo '<td><input type="checkbox" id="' . $value["id"] . '" name="' . $value["id"] . '" value="blockedUsers"></td>';
                 echo '</tr>';
-                }
-                ?>
+            }
+            ?>
         </table>
+        <div id="buttons_admin">
+            <button type="submit" name="btn_unblock">Desbloquear seleccionados</button>
+            <button type="submit" name="btn_unblockAll">Desbloquear todos</button>
+        </div>
     </div>
 <?php
 }
